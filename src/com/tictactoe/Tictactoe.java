af95d5477ea87836857691e4009bc7aefe4546b9
1501299026 <sinumsd98@gmail.com>
2021-05-19 12:17:09 -0700

    ADDED: CreateBoard Added

--- src/com/tictactoe/Tictactoe.java
+++ src/com/tictactoe/Tictactoe.java
@@ -3,7 +3,17 @@ package com.tictactoe;
 public class Tictactoe {
 
 	public static void main(String[] args) {
-     System.out.println("Welcome to tictactoe game");
+		System.out.println("Welcome in Tic tac toe game");
+		char[] board = createBoard();
+	}
+	public static char[] createBoard()
+	{
+		char[] board = new char[10];
+		for(int i = 1; i < board.length; i++)
+		{
+			board[i] = ' ';
+		}
+		return board;
 
 	}
 
